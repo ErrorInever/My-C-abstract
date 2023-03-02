aa6473486559300d09a399d7d804f1987ae7745e
ErrorInever <errorinever@gmail.com>
2023-03-02 09:17:18 +0000

    add logical and bitwise operations

---
+++ BasicOperations/Logic_operations/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mirage/Projects/CppRoadMap/BasicOperations/Logic_operations
+# Build directory: /home/mirage/Projects/CppRoadMap/BasicOperations/Logic_operations/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
