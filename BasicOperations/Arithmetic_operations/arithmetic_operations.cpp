4264442069c452b14737d5bf6c47e2e9d96db44f
ErrorInever <errorinever@gmail.com>
2023-03-02 08:08:23 +0000

    refactoring

--- BasicOperations/Arithmetic_operations/arithmetic_operations.cpp
+++ BasicOperations/Arithmetic_operations/arithmetic_operations.cpp
@@ -2,7 +2,19 @@
 
 
 int main(int, char**) {
-    int a {15};
-    int b {4};
-    std::cout << a / b << std::endl;
+    int a = 10;
+    int b = 6;
+
+    float c = 15.3;
+    double d = 3.4;
+
+    std::cout << "Binary operations: \n";
+    std::cout << a << " + " << b << " = " << a + b << std::endl;
+    std::cout << a << " - " << b << " = " << a - b << std::endl;
+    std::cout << a << " / " << b << " = " << a / b << std::endl;
+    std::cout << c << " / " << d << " = " << c / d << std::endl;
+    std::cout << a << " / " << d << " = " << a / d << std::endl;
+    std::cout << a << " % " << b << " = " << a % b << std::endl;
+
+    return 0;
 }
