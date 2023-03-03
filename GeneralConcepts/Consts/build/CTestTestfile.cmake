bc6e4db40ca4c8f73566b237398b904c262d09a4
ErrorInever <errorinever@gmail.com>
2023-03-03 17:27:52 +0000

    add consts chapter

---
+++ GeneralConcepts/Consts/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/mirage/Projects/CppRoadMap/GeneralConcepts/Consts
+# Build directory: /home/mirage/Projects/CppRoadMap/GeneralConcepts/Consts/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
