9cdd0ad914a7bc3e0d0af895923e6292eb0a8da5
ErrorInever <errorinever@gmail.com>
2023-03-04 12:31:40 +0000

    .gitignore update

--- BasicOperations/Arithmetic_operations/build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mirage/Projects/CppRoadMap/BasicOperations/Arithmetic_operations
-# Build directory: /home/mirage/Projects/CppRoadMap/BasicOperations/Arithmetic_operations/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
