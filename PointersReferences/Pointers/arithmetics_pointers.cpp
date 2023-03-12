4c036d2f6c0124a7b68ab67638edeb7804284a83
ErrorInever <errorinever@gmail.com>
2023-03-12 18:56:11 +0000

    Added arithmetics pointers

--- PointersReferences/Pointers/arithmetics_pointers.cpp
+++ PointersReferences/Pointers/arithmetics_pointers.cpp
@@ -5,6 +5,6 @@ int main(int, char**) {
     int array[5] = {10, 21, 33, 45, 51};
     int* arrPtr = array + 3;
     printf("%d\n", *arrPtr); 
-    printf("%d\n", arrPtr[+1]);
+    printf("%d\n", arrPtr[-1]);
     return 0;
 }
