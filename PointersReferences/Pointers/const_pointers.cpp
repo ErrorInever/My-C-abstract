61ed40ea056c50672df1d74d14f2be1e11e6d091
ErrorInever <errorinever@gmail.com>
2023-03-20 19:54:43 +0000

    Added const pointers chapter

--- PointersReferences/Pointers/const_pointers.cpp
+++ PointersReferences/Pointers/const_pointers.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 
 int main(int, char**) {
+    int value {5};
+
+    // pointer to "const int"
+    const int* ptr1 { &value };
+
+    // const pointer to "int"
+    int* const ptr2 { &value };
+
+    // const pointer to "const int"
+    const int* const ptr3 { &value };
 
     return 0;
 }
