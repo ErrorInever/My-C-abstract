ac1cdd68d3903e355494b664483b44dc55a064ac
ErrorInever <errorinever@gmail.com>
2023-03-23 19:24:32 +0300

    Updated references chapter

--- PointersReferences/References/main.cpp
+++ PointersReferences/References/main.cpp
@@ -4,5 +4,14 @@ int main(int, char**) {
     int value {5};
     int& refval {value}; // references to value
 
-    // references must be init
+    const int value_1 {4};
+    const int& refval_1 {value_1}; // references to const value
+
+    int x {1};
+    const int& ref1 {x}; // l-value
+
+    const int y {2};
+    const int& ref2 {y}; // const l-value
+
+    const int& ref3 {3}; // r-value
 }
